23eebdb074c542e977992df783d13d6bd3b06c63
Jordan Hamilton <Jordanh6489@hotmail.com>
2022-11-15 21:14:13 -0500

    Program Complete

--- InClass5PT1/Program.cs
+++ InClass5PT1/Program.cs
@@ -8,8 +8,29 @@ namespace InClass5PT1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            String userInput;
+            List<String> output = new List<String>();
+
+            // ask user for input
+            Console.WriteLine("In one word Please enter your first and Last Name!");
+            userInput = Console.ReadLine();
+
+            //flips the input string
+            for (int i = userInput.Length-1; i >= 0; i--)
+            {
+                output.Add((userInput[i].ToString()));
+            }
+
+            //prints the output
+            for (int i = 0; i <= output.Count-1;i++)
+            {
+                Console.Write(output[i]);
+            }
+
+            Console.WriteLine("Press Any Key to Escape!");
+            Console.ReadKey();
         }
     }
 }
